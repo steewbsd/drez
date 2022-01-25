6ceb6d8af99025716ca36f69ff67ccac01aa7435
steew0x8 <steew0x8@protonmail.com>
2022-01-25 22:46:12 +0100

    perform basic move validity
    Ignore-this: e365f87cb51da47b369441b75ea780f2b87ad34441ee4d52ab9dfdfa5f50a0e6df23b411efd19428
    
    darcs-hash:c11d3509e66c52dd845c7a2f246887d50acc4ec7

--- src/board.c
+++ src/board.c
@@ -24,42 +24,49 @@ board *init_board(char *fen) {
   
   /* TODO: interpret FEN notation and initialize game with it */
   /* a standard game will be created as as placeholder */
-  game->game[0][0] = (moveable){game->rook, WHITE, NORMAL, 0};
-  game->game[0][1] = (moveable){game->knight, WHITE, SKIP, 0};
-  game->game[0][2] = (moveable){game->bishop, WHITE, NORMAL, 0};
-  game->game[0][3] = (moveable){game->queen, WHITE, NORMAL, 0};
-  game->game[0][4] = (moveable){game->king, WHITE, NORMAL, 0};
-  game->game[0][5] = (moveable){game->bishop, WHITE, NORMAL, 0};
-  game->game[0][6] = (moveable){game->knight, WHITE, SKIP, 0};
-  game->game[0][7] = (moveable){game->rook, WHITE, NORMAL, 0};
+ game->game[0][0] = (moveable){game->rook, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][1] = (moveable){game->knight, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][2] = (moveable){game->bishop, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][3] = (moveable){game->queen, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][4] = (moveable){game->king, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][5] = (moveable){game->bishop, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][6] = (moveable){game->knight, WHITE, FLAG_FIRSTMOVE};
+ game->game[0][7] = (moveable){game->rook, WHITE, FLAG_FIRSTMOVE};
 
-  game->game[7][0] = (moveable){game->rook, BLACK, NORMAL, 0};
-  game->game[7][1] = (moveable){game->knight, BLACK, SKIP, 0};
-  game->game[7][2] = (moveable){game->bishop, BLACK, NORMAL, 0};
-  game->game[7][3] = (moveable){game->queen, BLACK, NORMAL, 0};
-  game->game[7][4] = (moveable){game->king, BLACK, NORMAL, 0};
-  game->game[7][5] = (moveable){game->bishop, BLACK, NORMAL, 0};
-  game->game[7][6] = (moveable){game->knight, BLACK, SKIP, 0};
-  game->game[7][7] = (moveable){game->rook, BLACK, NORMAL, 0};
+ game->game[7][0] = (moveable){game->rook, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][1] = (moveable){game->knight, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][2] = (moveable){game->bishop, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][3] = (moveable){game->queen, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][4] = (moveable){game->king, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][5] = (moveable){game->bishop, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][6] = (moveable){game->knight, BLACK, FLAG_FIRSTMOVE};
+ game->game[7][7] = (moveable){game->rook, BLACK, FLAG_FIRSTMOVE};
 
-  game->game[1][0] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][1] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][2] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][3] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][4] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][5] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][6] = (moveable){game->pawn, WHITE, NORMAL, 0};
-  game->game[1][7] = (moveable){game->pawn, WHITE, NORMAL, 0};
+ game->game[1][0] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][1] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][2] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][3] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][4] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][5] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][6] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
+ game->game[1][7] = (moveable){game->pawn, WHITE, FLAG_FIRSTMOVE};
 
-  game->game[6][0] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][1] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][2] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][3] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][4] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][5] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][6] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  game->game[6][7] = (moveable){game->pawn, BLACK, NORMAL, 0};
-  
+ game->game[6][0] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][1] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][2] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][3] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][4] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][5] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][6] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+ game->game[6][7] = (moveable){game->pawn, BLACK, FLAG_FIRSTMOVE};
+
+ /* init empty cells */
+ for (int i = 2; i < 7; i++) {
+   for (int j = 0; i < 8; j++) {
+	 game->game[i][j] = (moveable){NULL, NONE, 0};
+   }
+ }
+ 
   return game;
 }
 
@@ -77,7 +84,7 @@ char *key_from_coords(position pos) {
   return key;
 }
 
-int move(position origin, position target) {
+int move(position origin, position target, board *game) {
   /* we will use a position instead of a piece, as we just store
    all the currently utilized cells (or positions) in a hash map,
   with a concatenated version of the rank and file as the key.
@@ -87,26 +94,38 @@ int move(position origin, position target) {
   bishops will just be stored as four entries in the hash map,
   with their position as the hashmap key, and the data they
   point to is the same *bishop variable. */
-  ENTRY t;
-  t.key = key_from_coords(origin);
-  ENTRY *piece = hsearch(t,FIND);
-  if (piece == NULL) {
-	puts("Illegal position encountered");
-	return -1;
-  }
-  moveable *mov = (moveable *)piece->data;
-  /* erase value in the original cell */
-  t.data = NULL;
-  hsearch(t, ENTER);
-  if (mov->mk == NORMAL) {
-	position * moves = mov->p->moves(origin); /* we get an array containing all possible moves */
-	int move = 0;
-	while (moves[move].rank != -1 && moves[move].file != -1) {
-	  
-	  moves++;
+  moveable origin_moveable = game->game[origin.rank][origin.file];
+  moveable target_moveable = game->game[target.rank][target.file];
+  if ( origin_moveable.piece == NULL) return -1; /* no piece in the selected cell, error */
+
+  if (origin_moveable.flags & FLAG_PIN) return -1; /* piece is pinned, so can't move */
+  position *valid_moves = origin_moveable.piece->moves(origin);
+  /* check if move is valid by traversing the list of cells the piece is able to move to at this current position */
+  int i, found_move = 0;
+  while (valid_moves->file != -1 && valid_moves->rank != -1) { /* check for sentinel value (end of array) */
+	piece *current_cell_piece = game->game[valid_moves[i].rank][valid_moves[i].file].piece; /* store current cell's piece
+																							   (can be NULL) */
+	if (current_cell_piece != NULL && current_cell_piece->ident != 'n') return -1; /* if it is not a knight, and pieces are
+																		  in the way, block the move */
+	if (target.file == valid_moves[i].file && target.rank == valid_moves[i].rank) { /* found a valid move */
+	  found_move = 1;
+	  break;
 	}
+	i++;
   }
-  
+  if (found_move == 0) return -1; /* no move was found, return */
+
+  target_moveable.piece = origin_moveable.piece; /* set the new position to point to the piece */
+  origin_moveable.piece = NULL; /* erase the piece from the origin */
+  target_moveable.side = origin_moveable.side; /* copy piece side */
+  /* toggle flags */
+  /* toggle first move to 0 */
+  target_moveable.flags = origin_moveable.flags & ~(FLAG_FIRSTMOVE);
+  /* clear up the rest ... */
+
+  /* TODO: make use of FLAG_FIRSTMOVE to check for pawn movements */
+  free (valid_moves);
+  return 0;
 }
 
 
--- src/board.h
+++ src/board.h
@@ -1,23 +1,25 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#define FLAG_PINNED 0x1
+#define FLAG_PIN       0x1 /* block piece when it would discover a check */
+#define FLAG_CHECK     0x2 /* block moves when king is in check (additional checking
+							  should be done for moves preventing check) */
+#define FLAG_FIRSTMOVE 0x4 /* for castles and pawn starts */
 
 #include "piece.h"
 #include <stdint.h>
 #define SIZE_STD 8
 
 enum FILE_NUMS { a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6, h = 7 };
-typedef enum { BLACK, WHITE } SIDE;
+typedef enum { BLACK, WHITE, NONE } SIDE;
 typedef enum {
   NORMAL,
   SKIP
 } MOVE_KIND; /* signals whether a piece (namely the knight) can ignore other
                                             pieces in its path */
 typedef struct {
-  piece *p;
-  SIDE s;
-  MOVE_KIND mk;
+  piece *piece;
+  SIDE side;
   uint8_t flags;
 } moveable;
 
@@ -31,11 +33,12 @@ typedef struct {
   int size_x; 
   int size_y; 
   /* TODO: piece location hash table */
-  /*            rank      file*/
+  /*            rank      file     */
   moveable game[SIZE_STD][SIZE_STD];
 } board;
 
 board *init_board();
+void free_board(board *game);
 char *key_from_coords(position pos);
-int move(position origin, position target);
+int move(position origin, position target, board *game);
 #endif
